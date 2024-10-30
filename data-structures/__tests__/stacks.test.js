9f058d39aa2b1a13feab5e23ebe10d9d712a5bc4
nc_stephen_corke <stephen.corke@northcoders.com>
2024-10-30 11:08:07 +0000

    add tests and logic for properties and inital testing for push method

--- data-structures/__tests__/stacks.test.js
+++ data-structures/__tests__/stacks.test.js
@@ -1,1 +1,47 @@
-// create test suite for stack data structure here
+const createStack = require('../stack');
+
+describe('createStack', () => {
+  test('check if returned object has a quantity property which is initialised with 0', () => {
+    const testStack = createStack();
+    expect(testStack.quantity).toBe(0);
+  });
+  test('check if returned object has a storage property which is initialised as an empty object', () => {
+    const testStack = createStack();
+    expect(testStack.storage).toEqual({});
+  });
+  test('check if returned object has a maxSize property which is initialised with the passed in value', () => {
+    const testStack = createStack(5);
+    expect(testStack.maxSize).toBe(5);
+    const testStackTwo = createStack(17);
+    expect(testStackTwo.maxSize).toBe(17);
+  });
+  test('check that the default value of maxSize in 3 if no value in passed in', () => {
+    const testStack = createStack();
+    expect(testStack.maxSize).toBe(3);
+  });
+  // A push method, which adds items to the stack storage, provided the stack is not already full.
+  // const testStack = createStack();
+  // testStack.push('apple');
+  // testStack.storage; // { 1 : 'apple' }
+  // testStack.push('orange');
+  // testStack.storage; // { 1 : 'apple', 2 : 'orange' }
+  describe('push method', () => {
+    test('check if returned object has a method of push', () => {
+      const testStack = createStack(5);
+      expect(typeof testStack.push).toBe('function');
+    });
+    test('check if the push method add the passed in item to the storage on a key of 1', () => {
+      const testStack = createStack();
+      // arrange
+      // act
+      const pushOutput = testStack.push('apple');
+      // assert
+      expect(pushOutput).toBe(undefined);
+      expect(testStack.storage).toEqual({ 1: 'apple' });
+      expect(testStack.quantity).toBe(1);
+    });
+    // what happens of we push another item?
+
+    // what happens if push an item when the maxStorage has been reached
+  });
+});
--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,5 +1,32 @@
-function createStack() {
+// push is invoked
+// we need the value
+// we need to mutate the storage object on the instance
+
+function push(item) {
+  // this is always the object that the method was called on
+  this.storage[1] = item;
+  this.quantity++;
+}
+
+function createStack(maxSize) {
+  if (!maxSize) {
+    maxSize = 3;
+  }
   // build your stack object inside this factory function
+
+  // 1. object instantiation
+  const stack = {};
+
+  // 2. object decoration
+  stack.quantity = 0;
+  stack.storage = {};
+  stack.maxSize = maxSize; // maxSize is 3 if no arg passed
+
+  // 3. add methods
+  stack.push = push;
+
+  // 4. return the object
+  return stack;
 }
 
 module.exports = createStack;
